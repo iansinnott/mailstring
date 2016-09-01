f96ef3c40536a0a71cb35c5850a5d7df741b9d0d
Ian Sinnott <ian@iansinnott.com>
2016-09-01 08:58:30 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src'),
+      }
+    ],
+  },
+};
